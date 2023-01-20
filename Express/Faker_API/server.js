e41afe3b60f9504f7e25483e57b9f86b362b304d
Eric-Barajas <barajaseric505@gmail.com>
2023-01-19 18:05:17 -0800

    init commit

---
+++ Express/Faker_API/server.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+// allow server to recieve .json (when someone makes a post route)
+app.use(express.json());
+
+const { faker } = require('@faker-js/faker');
+// we can create a function to return a random / fake "Product"
+
+// ------------ First Function --------------//
+const createUser = () => {
+    const newUser = {
+        password: faker.internet.password(),
+        email: faker.internet.email(),
+        phoneNumber: faker.phone.phoneNumber(),
+        lastName: faker.name.lastName(),
+        firstName: faker.name.firstName(),
+        _id: faker.datatype.uuid()
+    };
+    return newUser;
+};
+
+
+// ------------ First Function --------------//
+
+// ------------ Second Function --------------//
+
+const createCompany = () => {
+    const newCompany = {
+        _id: faker.datatype.uuid(),
+        name: faker.company.companyName(),
+        address: {
+            street: faker.address.street(),
+            city: faker.address.city(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCode(),
+            country: faker.address.country()
+        }
+    };
+    return newCompany;
+};
+
+
+// ------------ Second Function --------------//
+
+// ------------ API routes --------------//
+
+app.get("/api/users/new", (req, res) => {
+    const newFakeUser = createUser();
+    res.json({ newFakeUser });
+})
+// app.get("/api/users/new", newUser)
+
+app.get("/api/companies/new", (req, res) => {
+    const newFakeCompany = createCompany();
+    res.json({ newFakeCompany });
+})
+
+app.get("/api/user/company", (req, res) => {
+    const newFakeCompany = createCompany();
+    const newFakeUser = createUser();
+    res.json({ newFakeCompany, newFakeUser });
+})
+
+// ------------ API routes --------------//
+
+
+// console.log(newFakeUser, newFakeCompany);
+
+// this needs to be below (the app.(somewthing) code blocks)the other code blocks
+app.listen(port, () => console.log(`Listening on port: ${port}`));
--- Express/HelloExpress/server.js
+++ Express/HelloExpress/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+// allow server to recieve .json (when someone makes a post route)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // req is short for request
@@ -10,20 +11,36 @@ app.get("/api", (req, res) => {
   res.send("Our express api server is now sending this over to the browser");
 });
 
+const apiCallBack = (req, res) => {
+  res.json({
+    message: "Hello world",
+    secretNumber: 42,
+    instructurNames: ["max", "Robert"],
+    myUser: {
+      name: "Max R",
+      job: "Instructor",
+      hairColor: "brown",
+      classesTaught: ["Python", "Mern", "C#"]
+    }
+  });
+}
+
+app.get("/goat", apiCallBack);
+
 // how to get form data
-// app.post("/api/users", (req, res) => {
-//   // req.body will contain the form data from Postman or from React
-//   console.log(req.body);
-//   // we can push it into the users array for now...
-//   // later on this will be inserted into a database
-//   users.push(req.body);
-//   // we always need to respond with something
-//   res.json({ status: "ok" });
-// });
+app.post("/api/users", (req, res) => {
+  // req.body will contain the form data from Postman or from React
+  console.log(req.body);
+  // we can push it into the users array for now...
+  // later on this will be inserted into a database
+  users.push(req.body);
+  // we always need to respond with something
+  res.json({ status: "ok" });
+});
+
+// this needs to be below (the app.(somewthing) code blocks)the other code blocks
+app.listen(port, () => console.log(`Listening on port: ${port}`));
 
 // const server = app.listen(8000, () =>
 //   console.log(`Server is locked and loaded on port ${server.address().port}!`)
 // );
-
-// this needs to be below the other code blocks
-app.listen(port, () => console.log(`Listening on port: ${port}`));
